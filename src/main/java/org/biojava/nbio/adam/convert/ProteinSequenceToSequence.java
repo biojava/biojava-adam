8c3635cd187455e0e4dd3a8d7aaa0bb553eebfce
Michael L Heuer <heuermh@acm.org>
2019-06-12 14:49:05 -0500

    update copyright years to include 2019

--- src/main/java/org/biojava/nbio/adam/convert/ProteinSequenceToSequence.java
+++ src/main/java/org/biojava/nbio/adam/convert/ProteinSequenceToSequence.java
@@ -1,7 +1,7 @@
 /*
 
     biojava-adam  Biojava and ADAM integration.
-    Copyright (c) 2017-2018 held jointly by the individual authors.
+    Copyright (c) 2017-2019 held jointly by the individual authors.
 
     This library is free software; you can redistribute it and/or modify it
     under the terms of the GNU Lesser General Public License as published
