47a4708b1a93bc3c907ab26b7edb37dc088a0dde
Michael L Heuer <heuermh@acm.org>
2019-06-23 16:04:36 -0500

    adding spark-shell scripts

---
+++ scripts/loadFastaProtein.scala
@@ -0,0 +1,48 @@
+/*
+
+    biojava-adam  BioJava and ADAM integration.
+    Copyright (c) 2017-2019 held jointly by the individual authors.
+
+    This library is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Lesser General Public License as published
+    by the Free Software Foundation; either version 3 of the License, or (at
+    your option) any later version.
+
+    This library is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; with out even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+    License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this library;  if not, write to the Free Software Foundation,
+    Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA.
+
+    > http://www.fsf.org/licensing/licenses/lgpl.html
+    > http://www.opensource.org/licenses/lgpl-license.php
+
+*/
+import org.slf4j.LoggerFactory
+val logger = LoggerFactory.getLogger("loadFastaProtein")
+
+import org.apache.log4j.{ Level, Logger }
+Logger.getLogger("loadFastaProtein").setLevel(Level.INFO)
+Logger.getLogger("org.biojava").setLevel(Level.INFO)
+
+import org.biojava.nbio.adam.BiojavaAdamContext
+val bac = BiojavaAdamContext(sc)
+
+val inputPath = Option(System.getenv("INPUT"))
+val outputPath = Option(System.getenv("OUTPUT"))
+
+if (!inputPath.isDefined || !outputPath.isDefined) {
+  logger.error("INPUT and OUTPUT environment variables are required")
+  System.exit(1)
+}
+
+val sequences = bac.loadBiojavaFastaProtein(inputPath.get)
+
+logger.info("Saving protein sequences to output path %s ...".format(outputPath.get))
+sequences.save(outputPath.get, asSingleFile = true, disableFastConcat = false)
+
+logger.info("Done")
+System.exit(0)
